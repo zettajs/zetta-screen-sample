a5dee7c35d2c08c52941bf0f6da3150aa7573b10
Kevin Swiber <kswiber@gmail.com>
2014-04-23 17:08:31 -0400

    Allowing wrapping of the screen.

--- ZettaScreen.ino
+++ ZettaScreen.ino
@@ -1,20 +1,8 @@
-// Draw Texts - Demonstrate drawChar and drawString
-//  This library is free software; you can redistribute it and/or
-//  modify it under the terms of the GNU Lesser General Public
-//  License as published by the Free Software Foundation; either
-//  version 2.1 of the License, or (at your option) any later version.
-//
-//  This library is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-//  Lesser General Public License for more details.
-//
-//  You should have received a copy of the GNU Lesser General Public
-//  License along with this library; if not, write to the Free Software
-//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
 #include <stdint.h>
 #include <SPI.h>
+//#include <Bridge.h>
+//#include <WiFi.h>
+#include "PubSubClient.h"
 #include "TouchScreen.h"
 #include "TFT.h"
 
@@ -32,16 +20,124 @@
   #define XP 57   // can be a digital pin, this is A3 
 #endif 
 
-void setup()
-{
+char ssid[] = "Loft21";
+char pass[] = "silkylotus997";
 
-Tft.init();  //init TFT library
+//int status = WL_IDLE_STATUS;
+
+//WiFiClient client;
+
+#define BUFFER_MAX 17
+#define SCREEN_LENGTH 16
+#define MAX_LINE_COUNT 11
+#define NEWLINE 0xa
+
+#define X_START 220
+#define Y_START 10
+
+int incomingByte = 0;
+char buffer[BUFFER_MAX+1];
+int index = 0;
+int charCount = 0;
+int x = X_START;
+int y = Y_START;
+int lineCount = 0;
+
+void setup() {
+  while(!Serial) {
+  }
+
+  //Bridge.begin();
+  Tft.init();
   Tft.setDisplayDirect(UP2DOWN);
-  Tft.drawString("Science, bitch!",220,20,2,WHITE);
-}
+  Tft.drawString("> Science, bitch!", x, y, 2, WHITE);
+  lineCount = lineCount + 1;
 
-void loop()
-{
+  Serial.begin(9600);
   
+  /*if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present.");
+    while(true);
+  }*/
+  
+  Serial.println("Starting...");
+  Serial.println();
 
+  resetBuffer();
+  
+  /*while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    
+    status = WiFi.begin(ssid, pass);
+    
+    delay(10000);
+  }
+  
+  printWifiStatus();*/
 }
+
+void loop() {
+  if (Serial.available() > 0) {
+    incomingByte = Serial.read();
+
+    if (incomingByte != NEWLINE) {
+      buffer[index] = incomingByte;
+    }
+
+    index = index + 1;
+
+    if (index == BUFFER_MAX || incomingByte == NEWLINE) {
+
+      if (lineCount == MAX_LINE_COUNT) {
+        resetScreen();
+      } else {
+        x = x - 20;
+      }
+
+      Serial.print("I received: ");
+      Serial.println(buffer);
+
+      Tft.drawString(buffer, x, y, 2, WHITE);
+      memset(buffer, 0, sizeof(buffer));
+
+      lineCount = lineCount + 1;
+
+      if (incomingByte == NEWLINE) {
+        resetBuffer();
+      } else {
+        index = 0;
+      }
+    }
+  }
+}
+
+void resetScreen() {
+  x = X_START;
+  Tft.paintScreenBlack();
+  lineCount = 0;
+}
+
+void resetBuffer() {
+  buffer[0] = '>';
+  buffer[1] = ' ';
+  index = 2;
+}
+
+/*
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}*/
